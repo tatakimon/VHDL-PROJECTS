ed894581c890bff3e2177c1af3f6775264f6a4c1
Kerem KARABIYIK <tatakimon@gmail.com>
2025-09-29 14:49:22 +0200

    Update ADT7420.vhd
    -

--- project I2C adt7420 communication/ADT7420.vhd
+++ project I2C adt7420 communication/ADT7420.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 	Mehmet Burak AYKENAR
--- 
--- Create Date: 07/02/2019 07:50:36 AM
--- Design Name: 
--- Module Name: ADT7420 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
