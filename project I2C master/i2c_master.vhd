7e75ad018801d9fc2c640cb00a7a1e56bc80c849
Kerem KARABIYIK <tatakimon@gmail.com>
2025-09-29 14:51:22 +0200

    Update i2c_master.vhd
    --

--- project I2C master/i2c_master.vhd
+++ project I2C master/i2c_master.vhd
@@ -1,38 +1,9 @@
---------------------------------------------------------------------------------
---
---   FileName:         i2c_master.vhd
---   Dependencies:     none
---   Design Software:  Quartus II 64-bit Version 13.1 Build 162 SJ Full Version
---
---   HDL CODE IS PROVIDED "AS IS."  DIGI-KEY EXPRESSLY DISCLAIMS ANY
---   WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
---   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
---   PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
---   BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
---   DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
---   PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
---   BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
---   ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
---
---   Version History
---   Version 1.0 11/01/2012 Scott Larson
---     Initial Public Release
---   Version 2.0 06/20/2014 Scott Larson
---     Added ability to interface with different slaves in the same transaction
---     Corrected ack_error bug where ack_error went 'Z' instead of '1' on error
---     Corrected timing of when ack_error signal clears
---   Version 2.1 10/21/2014 Scott Larson
---     Replaced gated clock with clock enable
---     Adjusted timing of SCL during start and stop conditions
---   Version 2.2 02/05/2015 Scott Larson
---     Corrected small SDA glitch introduced in version 2.1
--- 
---------------------------------------------------------------------------------
+
 
 -- MBA --
 
--- KOLAY KULLANIM ÝÇÝN
--- busy sinyali high olduðu zaman "ena" sinyali high ise consecutive iþlem yapar
+-- KOLAY KULLANIM ÃÃ‡ÃN
+-- busy sinyali high olduÃ°u zaman "ena" sinyali high ise consecutive iÃ¾lem yapar
 
 	-- EXAMPLE CODE 
 	-- when PWRCTL =>
